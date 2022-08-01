55f5ae903aa05dc4b96593d9efd6b03a432a6862
christopher <cdnance1829@gmail.com>
2022-07-31 21:01:05 -0400

    Completed Assignment

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -6,11 +6,26 @@ class Person
   end
 
   def get_married(person)
+    if person.is_a?(Person)
     self.partner = person
     person.partner = self
+    else
+      begin
+      raise PartnerError
+      rescue PartnerError => error
+        puts error.message
+    end
   end
 
+class PartnerError < StandardError
+  def message
+      "You must the get_married method an arguement of an instance of the person class!"
+  end
 end
+
+
+
+
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
